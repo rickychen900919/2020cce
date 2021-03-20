83378407bc6b5dbfb83808cb541cd33604487d19
rickychen900919 <80501481+rickychen900919@users.noreply.github.com>
2021-03-20 21:47:13 +0800

    Update 02.cpp

--- week01/02.cpp
+++ week01/02.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int n=10,r=0,a[10];
+   for(int i=0;i<n;i++)
+   {
+     scanf("%d",&a[10]);
+     if(a[10]%3==0) r++;
 
+   }
+
+
+       printf("%d\n",r);
+}
