81e30bb5fb4808ae5027bf25608abe2622e3786a
rickychen900919 <80501481+rickychen900919@users.noreply.github.com>
2021-03-20 21:52:59 +0800

    Update 03.cpp

--- week02/03.cpp
+++ week02/03.cpp
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int fun(int n)
+{
+  int ans=0;
+  for(int i=n;i>=1;i--){
+     ans=(i-1)*i+ans;
 
+
+  }
+
+
+   return ans;
+
+
+
+}
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   printf("%d\n",fun(n));
+
+
+
+}
